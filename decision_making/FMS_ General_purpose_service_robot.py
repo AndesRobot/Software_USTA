5858a81435bee08ffddf8bffd4bea39e5b5549b9
Juan Diego Baez Guerrero <juanbaezg@usantotomas.edu.co>
2025-06-08 13:53:26 -0500

    Update FMS_ General_purpose_service_robot.py

--- decision_making/FMS_ General_purpose_service_robot.py
+++ decision_making/FMS_ General_purpose_service_robot.py
@@ -1,81 +1,56 @@
-def q1():
-    print("Q1: Se detecta obstáculo")
+ef q1():
+    print("Q1: Mantenerse quieto")
     while True:
         char = yield
-        if char == 'a':
-            yield from q1()
-        elif char == 'b':
+        if char == 'a': 
             yield from q2()
         else:
             break
 
 def q2():
-    print("Q2: No se detecta obstáculo")
+    print("Q2: Recibir orden")
     while True:
         char = yield
-        if char == 'c':
-            yield from q1()
-        elif char == 'd':
-            yield from q2()
-        elif char == 'm':
+        if char == 'b':  # Evento: Persona da orden
             yield from q3()
         else:
             break
 
 def q3():
-    print("Q3: Quieto")
+    print("Q3: Ejecutar orden")
     while True:
         char = yield
-        if char == 'e':
-            yield from q3()
-        elif char == 'f':
+        if char == 'c':  # Evento: Orden recibida
             yield from q4()
         else:
             break
 
 def q4():
-    print("Q4: Se recibe orden")
+    print("Q4: Finalizar orden")
     while True:
         char = yield
-        if char == 'g':
-            yield from q3()
-        elif char == 'h':
+        if char == 'd':  # Evento: Navegación al punto de instrucción
             yield from q5()
         else:
             break
 
 def q5():
-    print("Q5: Ejecutando orden")
+    print("Q5: Llegar a instrucción")
     while True:
         char = yield
-        if char == 'i':
+        if char == 'e':  # Evento: Punto de instrucción alcanzado
             yield from q6()
-        elif char == 'z':
+        elif char == 'f':  # Evento: Objeto detectado
             yield from q1()
         else:
             break
 
 def q6():
-    print("Q6: Finalizando orden")
+    print("Q6: Detectar obstáculos")
     while True:
         char = yield
-        if char == 'j':
-            yield from q7()
-        elif char == 'z':
-            yield from q1()
-        else:
-            break
-
-def q7():
-    print("Q7: Punto de instrucción")
-    while True:
-        char = yield
-        if char == 'z':
-            yield from q1()
-        elif char == 'k':
-            yield from q4()
-        elif char == 'l':
-            yield from q3()
+        if char == 'g':  # Evento: Objeto evadido
+            yield from q5()
         else:
             break
 
@@ -84,7 +59,7 @@ def fsm():
     next(state)
     while True:
         try:
-            char = input("Presiona una tecla (a-l, z) o 'exit' para salir: ").lower()
+            char = input("Presiona una tecla (a–g) o 'exit' para salir: ").lower()
             if char == 'exit':
                 break
             state.send(char)

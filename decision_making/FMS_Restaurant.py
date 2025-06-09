702b2cad71cc3dd30e9c08c79bca133f2ec6732d
Juan Diego Baez Guerrero <juanbaezg@usantotomas.edu.co>
2025-06-09 18:10:26 -0500

    Update FMS_Restaurant.py

--- decision_making/FMS_Restaurant.py
+++ decision_making/FMS_Restaurant.py
@@ -8,108 +8,108 @@ def q1():
             break
 
 def q2():
-    print("Q2: Going to table")
+    print("Q2: Watching queue")
     while True:
         char = yield
         if char == 'b':
             yield from q2()
         elif char == 'c':
             yield from q3()
-        elif char == 'd':
-            yield from q5()
-        elif char == 'e':
-            yield from q7()
         else:
             break
 
 def q3():
-    print("Q3: Looking at table")
+    print("Q3: Going to customer table")
     while True:
         char = yield
-        if char == 'f':
-            yield from q4()
-        elif char == 'g':
+        if char == 'd':
             yield from q3()
+        elif char == 'e':
+            yield from q4()
+        elif char == 'f':
+            yield from q11()
         else:
             break
 
 def q4():
-    print("Q4: Classifying object")
+    print("Q4: Taking order")
     while True:
         char = yield
-        if char == 't':
-            yield from q11()
-        elif char == 'h':
-            yield from q2()
+        if char == 'g':
+            yield from q5()
         else:
             break
 
 def q5():
-    print("Q5: Picking object")
+    print("Q5: Confirming order")
     while True:
         char = yield
-        if char == 'i':
+        if char == 'h':
             yield from q6()
         else:
             break
 
 def q6():
-    print("Q6: Navigating to destination")
+    print("Q6: Returning to kitchen")
     while True:
         char = yield
-        if char == 'j':
-            yield from q2()
-        elif char == 's':
+        if char == 'i':
+            yield from q7()
+        elif char == 'z':
             yield from q11()
         else:
             break
 
 def q7():
-    print("Q7: Dropping object")
+    print("Q7: Picking up order")
     while True:
         char = yield
-        if char == 'm':
+        if char == 'k':
             yield from q8()
-        if char == 'l':
-            yield from q7()
         else:
             break
 
 def q8():
-    print("Q8: Verifying table items")
+    print("Q8: Delivering order")
     while True:
         char = yield
-        if char == 'n':
-            yield from q8()
-        elif char == 'o':
+        if char == 'l':
             yield from q9()
-        else:
-            break
-
-def q9():
-    print("Q9: Completing task")
-    while True:
-        char = yield
-        if char == 'p':
+        elif char == 'm':
             yield from q10()
         else:
             break
 
-def q10():
-    print("Q10: Detecting obstacle")
+def q9():
+    print("Q9: Ending shift")
     while True:
         char = yield
-        if char == 'q':
+        if char == 'n':
             yield from q2()
         else:
             break
 
+def q10():
+    print("Q10: Requesting instructions")
+    while True:
+        char = yield
+        if char == 'o':
+            yield from q2()
+        elif char == 'p':
+            yield from q10()
+        else:
+            break
+
 def q11():
-    print("Q11: Obstacle detected")
+    print("Q11: Handling obstacle")
     while True:
         char = yield
         if char == 'r':
-            yield from q2()
+            yield from q6()
+        elif char == 's':
+            yield from q8()
+        elif char == 'q':
+            yield from q3()
         else:
             break
 
